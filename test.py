314a666b74a5a0dee62f397c8eb07c36464a6648
Tzekingo <2433297831@qq.com>
2025-10-08 21:15:19 +0800

    。。

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ config, datasets = construct_problem_set(config)
 test_problem_set = datasets[1]
 test_problem_set = [p[0] for p in test_problem_set]
 n_problem = len(test_problem_set)
-max_fes = 10000
+max_fes = 100
 my_noise = Gaussian_noise(begin_std=0.0, end_std=10.0)
 ps = 4
 population_weight = []
@@ -26,10 +26,15 @@ for _ in range(ps):
     random_subprob_weight = np.zeros(n_problem)
     random_index = np.random.randint(0, n_problem)
     random_subprob_weight[random_index] = 1
-    subprob_weight = Sub_Problem_Weight(n_problem, random_subprob_weight, max_fes, rates=0.2)
-    population_weight.append(subprob_weight)
+    population_weight.append(random_subprob_weight)
+population_weight = Sub_Problem_Weight(n_problem, population_weight, max_fes, rates=0.02)
 problem_set = Dynamic_Problem(test_problem_set, population_weight, my_noise, max_fes)
+
 print(problem_set.eval(random_x))
+print(problem_set.population_weight.get_weight())
+print(problem_set.eval(random_x))
+print(problem_set.population_weight.get_weight())
+
 
 
 
